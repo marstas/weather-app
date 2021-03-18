76465c31da3d78f5ed6161173ba0209e2b368afd
marstas <marstas@gmail.com>
2021-03-18 03:55:54 +0200

    removed static mapping logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import {getCityId} from "./utils";
 import "./App.scss";
 
-const App: React.FC = () => <h1>{getCityId("Vilnius")}</h1>;
+const App: React.FC = () => <h1>hi</h1>;
 
 export default App;
