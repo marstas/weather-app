715ac02f704b21442e39d92d49cf74acb2698fa9
marstas <marstas@gmail.com>
2021-03-19 11:43:58 +0200

    update ipFindApiKey

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const weatherMapApiKey = "86e9a453e4af0c55cc11e8bf54c567d0";
-const ipFindApiKey = "52689920-907f-47ba-9113-36bf31106896";
+const ipFindApiKey = "54151f53-0664-4a57-992c-ecb0f474aaf5";
 const current = `https://api.openweathermap.org/data/2.5/weather?appid=${weatherMapApiKey}`;
 const onecall = `https://api.openweathermap.org/data/2.5/onecall?appid=${weatherMapApiKey}&exclude=current,minutely,hourly,alerts`;
 const imgBase = "https://openweathermap.org/img/wn/";
