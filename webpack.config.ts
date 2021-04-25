19ac074f09f3cd1a55b6a74cd6df492afd5316e1
marstas <marstas@gmail.com>
2021-04-25 09:42:54 +0300

    Refactor webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,73 +4,73 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
 
-type Env = {
+type Environment = {
   production: boolean;
   development: boolean;
 };
 
-const webpackConfig = (env: Env): Configuration => ({
-  entry: "./src/index.tsx",
-  ...(env.production || !env.development ? {} : { devtool: "eval-source-map" }),
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    //TODO waiting on https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/61
-    //@ts-ignore
-    plugins: [new TsconfigPathsPlugin()]
-  },
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "build.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        options: {
-          transpileOnly: true
+export default function webpackConfig(env: Environment): Configuration {
+  return {
+    entry: "./src/index.tsx",
+    ...(env.production || !env.development ? {} : { devtool: "eval-source-map" }),
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      //TODO waiting on https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/61
+      //@ts-ignore
+      plugins: [new TsconfigPathsPlugin()]
+    },
+    output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "build.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          },
+          exclude: /dist/
         },
-        exclude: /dist/
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "svg-url-loader",
-            options: {
-              limit: 10000
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "svg-url-loader",
+              options: {
+                limit: 10000
+              }
             }
-          }
-        ]
-      }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      }),
+      new webpack.DefinePlugin({
+        "process.env.PRODUCTION": env.production || !env.development,
+        "process.env.NAME": JSON.stringify(require("./package.json").name),
+        "process.env.VERSION": JSON.stringify(require("./package.json").version)
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        eslint: {
+          files: "./src/**/*.{ts,tsx,js,jsx}" // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+        }
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new webpack.DefinePlugin({
-      "process.env.PRODUCTION": env.production || !env.development,
-      "process.env.NAME": JSON.stringify(require("./package.json").name),
-      "process.env.VERSION": JSON.stringify(require("./package.json").version)
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      eslint: {
-        files: "./src/**/*.{ts,tsx,js,jsx}" // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
-      }
-    })
-  ]
-});
-
-export default webpackConfig;
+  };
+}
