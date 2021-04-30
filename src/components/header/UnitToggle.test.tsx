47c3855d2638891e20b3e24c614328fed84515cf
marstas <marstas@gmail.com>
2021-04-30 22:59:53 +0300

    Refactor UnitToggle tests to use test.each()

--- src/components/header/UnitToggle.test.tsx
+++ src/components/header/UnitToggle.test.tsx
@@ -4,24 +4,16 @@ import UnitToggle from "./UnitToggle";
 
 const onUnitChangeMock = jest.fn();
 
-test("renders imperial values", () => {
-  render(<UnitToggle system="imperial" isChecked={false} onUnitChange={onUnitChangeMock} />);
+test.each([
+  ["imperial", "toggle-f", " ° F"],
+  ["metric", "toggle-c", " ° C"]
+])("renders %s system elements", (unitSystem, toggleId, labelText) => {
+  render(<UnitToggle system={unitSystem} isChecked={false} onUnitChange={onUnitChangeMock} />);
 
   const radio = screen.getByTestId("unit-radio") as HTMLInputElement;
   const label = screen.getByTestId("unit-label") as HTMLLabelElement;
-  expect(radio.id).toBe("toggle-f");
-  expect(radio.name).toBe("toggle-f");
-  expect(label.htmlFor).toBe("toggle-f");
-  expect(label.innerHTML).toBe(" ° F");
-});
-
-test("renders metric values", () => {
-  render(<UnitToggle system="metric" isChecked={false} onUnitChange={onUnitChangeMock} />);
-
-  const radio = screen.getByTestId("unit-radio") as HTMLInputElement;
-  const label = screen.getByTestId("unit-label") as HTMLLabelElement;
-  expect(radio.id).toBe("toggle-c");
-  expect(radio.name).toBe("toggle-c");
-  expect(label.htmlFor).toBe("toggle-c");
-  expect(label.innerHTML).toBe(" ° C");
+  expect(radio.id).toBe(toggleId);
+  expect(radio.name).toBe(toggleId);
+  expect(label.htmlFor).toBe(toggleId);
+  expect(label.innerHTML).toBe(labelText);
 });
